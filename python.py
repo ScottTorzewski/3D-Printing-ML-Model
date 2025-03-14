eeeacae72d98bdf97ab9b0c99496e013ee39e57c
Scott Torzewski <torzewskis@gmail.com>
2025-03-14 12:12:53 -0400

    Update python.py

--- python.py
+++ python.py
@@ -1,2 +1,19 @@
-# Ensures Github Detects Python for Statistics
-print("Hello, GitHub!")
+# Separate Python Code for Github Statistics
+
+# Import libraries
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
+import xgboost as xgb
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.model_selection import cross_val_score
+import joblib
+from sklearn.linear_model import Ridge, Lasso
+from sklearn.model_selection import GridSearchCV
+import shap
